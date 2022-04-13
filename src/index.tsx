1b852b2f34838e15b406149f5332c3cf07117fbb
Leno <akrachunov@gmail.com>
2022-04-13 20:54:30 +0100

    Events

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom/client";
 import GuestList from "./state/GuestList";
 import UserSearch from "./state/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 const App = () => {
     return (
         <div>
             <GuestList />
             <UserSearch />
+            <EventComponent />
         </div>
     );
 };
